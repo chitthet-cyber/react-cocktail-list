bfb3f4c47f110d05f96eb278c3df7e19ca1ea11c
chitthet-cyber <fernandoaung28@gmail.com>
2020-12-20 10:55:30 +0630

    first commit

---
+++ src/pages/About.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti doloremque eos sunt accusantium deserunt id exercitationem cupiditate dolores fugiat minima error odio sit, quod, molestiae perspiciatis quidem maxime dolorem animi.
+    </p>
+    </section>
+  );
+};
+
+export default About;
